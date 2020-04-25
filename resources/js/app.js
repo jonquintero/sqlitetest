3d3a1c2d0039bad02087acf2d5e2f5e41585c64f
Jonathan Quintero <jonquintero@hotmail.com>
2020-04-25 07:06:18 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+window.$ = window.jQuery = require('jquery');
+
+require('popper.js');
+require('./bootstrap');
+require('perfect-scrollbar');
+require('@coreui/coreui');
+require('datatables.net-bs4');
+require('select2');
+window.Swal  = require('sweetalert2');
+window.axios = require('axios');
+window.Vue   = require('vue');
